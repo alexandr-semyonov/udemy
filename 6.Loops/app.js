901f2a3f0015ef41bbe62c4803afda1da9818752
Semyonov Alexandr <alexandrbublik@gmail.com>
2020-10-29 18:13:04 +0200

    complete task loops

--- 6.Loops/app.js
+++ 6.Loops/app.js
@@ -33,3 +33,52 @@ for (let i = num; i >= 1; i--) {
 }
 
 console.log(`Factorial of number ${num} is ${numFactorial}`);
+
+/*4. На основе строки “JavaScript is a pretty good language” сделать новую строку, где каждое слово начинается с большой буквы, а пробелы удалены. Использовать for. */
+
+const str3 = 'JavaScript is a pretty good language';
+let resultStr = '';
+for (let i = 0; i < str3.length; i++){
+  if (str3[i] === 0 || str3[i - 1] === ' '){    
+    resultStr += str3[i].toUpperCase();
+  } else if (str3[i] === ' '){
+    resultStr += '';
+  } else {
+    resultStr += str3[i];
+  }
+}
+
+console.log(resultStr);
+
+
+/*5. Найти все нечетные числа в массиве от 1 до 15 включительно и вывести их в консоль. Массив [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] Использовать for of. */
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+for (let value of arr){
+  if (value%2 === 0){console.log(value);
+  }  
+}
+
+/*6. Дан объект:
+let list = {
+name: ‘denis’,
+work: ‘easycode’,
+age: 29
+}
+
+Перебрать объект и если значение в свойстве это строка то переписать ее всю в верхнем регистре. Использовать for in. */
+
+let list = {
+  name: 'denis',
+  work: 'easycode',
+  age: 29
+}
+
+for (let key in list){
+  if (typeof list[key] === 'string'){
+    list[key] = list[key].toUpperCase();
+  }  
+}
+
+console.log(list);
